81538ac25a521834399dea8f5448a12d9329c98b
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-26 12:08:54 -0300

    bootstrap added

---
+++ src/app/pages/boards/boards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boards',
+  standalone: true,
+  imports: [],
+  templateUrl: './boards.component.html',
+  styles: ``
+})
+export class BoardsComponent {
+
+}
